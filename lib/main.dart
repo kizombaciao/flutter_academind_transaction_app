a640172d410a070a6a0c2e441a1e2163651dc0ee
Kizomba <kizomba@github.com>
2021-03-10 17:21:37 +0100

    83

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import './transactions.dart';
 
@@ -88,7 +89,7 @@ class MyHomePage extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          tx.date.toString(),
+                          DateFormat.yMMMd().format(tx.date),
                           style: TextStyle(
                             fontSize: 12,
                             color: Colors.grey,
