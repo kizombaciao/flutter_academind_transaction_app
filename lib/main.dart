d6e1278ae7aaf6772b7f11cb08747378b7bc567b
Kizomba <kizomba@github.com>
2021-03-10 17:38:43 +0100

    84

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyHomePage extends StatelessWidget {
         title: Text('Flutter App'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        //mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
@@ -48,6 +48,29 @@ class MyHomePage extends StatelessWidget {
               color: Colors.blue,
               child: Text('CHART!'),
               elevation: 50,
+            ),
+          ),
+          Card(
+            elevation: 5,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Title'),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Amount'),
+                  ),
+                  TextButton(
+                    child: Text('Add Transaction'),
+                    onPressed: () {
+                      //print('aaa');
+                    },
+                  )
+                ],
+              ),
             ),
           ),
           Column(
